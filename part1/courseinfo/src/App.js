b39508005affe3512d968a3d341774d72d427629
kazumaneet <mekbibtare@gmail.com>
2021-11-26 18:14:40 +0300

    completed part 5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -11,7 +11,7 @@ const Content = (props) =>{
       <div>
         {props.parts.map(part =>{
           return(
-          <Part part = {part.name} exercises = {part.exercises}/>
+          <Part key = {part.name} part = {part.name} exercises = {part.exercises}/>
         )})}
       </div>
     )
@@ -33,26 +33,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [ {
-    name : 'Fundamentals of React',
-    exercises : 10
-    },
-    {
-    name :'Using props to pass data',
-    exercises:7
-    },
-    {
-    name: 'State of a component',
-    exercises:14    
-    }
-  ]
+  const course ={
+    name: 'Half Stack application development',
+    parts : [ 
+      {
+      name : 'Fundamentals of React',
+      exercises : 10
+      },
+      {
+      name :'Using props to pass data',
+      exercises:7
+      },
+      {
+      name: 'State of a component',
+      exercises:14    
+      }
+    ]
+  }
   return (
     <>
 
-      <Header name ={course}/>
-      <Content parts = {parts}/>
-      <Total parts = {parts}/>
+      <Header name ={course.name}/>
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
 
     </>
   )
