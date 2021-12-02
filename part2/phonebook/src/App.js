2cc12b8673195c908dce949221269bb55ab856a0
kazumaneet <mekbibtare@gmail.com>
2021-12-02 20:42:20 +0300

    Implmented a way to add names

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const PhoneNumber = ({name}) => {
+  return(
+      <li>{name}</li>
+    )
+}
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  
+  const addNewName = (e) =>{
+    e.preventDefault();
+    const person = {
+      name:newName
+    }
+    setPersons(persons.concat(person));
+    setNewName('');
+  }
+  const getNewName = (e) =>{
+    setNewName(e.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit = {addNewName}>
+        <div>
+          name: <input onChange = {getNewName} value = {newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+     
+      {persons.map(person =><PhoneNumber key={person.name} name = {person.name}/>)}
+     
+      ...
+    </div>
+  )
+}
+
+export default App
