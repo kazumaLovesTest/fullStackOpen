bda671720741e089fa2a1b65342c0b7dbe68804b
kazumaneet <mekbibtare@gmail.com>
2021-12-03 11:14:02 +0300

    Implemented a search feature

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,32 @@
 import React, { useState } from 'react'
 
-const Contact = ({name,number}) => {
+const Contact = ({persons,filteredContact}) => {
+  if (filteredContact === '')
   return(
-      <li>{name} {number}</li>
+     <> 
+      {persons.map(person=><li key = {person.id}>{person.name} {person.number}</li>)}
+    </>
     )
+  else return(
+    <>
+    {persons.filter(person =>person.name.toLocaleLowerCase().includes(filteredContact.toLowerCase()))
+    .map(person=><li key = {person.id}>{person.name} {person.number}</li>)}
+    </>
+  )
 }
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '928440178'
-  }])
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
 
-  const [newName, setNewName] = useState('')
-  const [newNumber,SetNewNumber] = useState('')
+  const [newName, setNewName] = useState('');
+  const [newNumber,SetNewNumber] = useState('');
+  const [filteredContact,setFilter] = useState('');
+  
 
   const addContact = (e) =>{
     e.preventDefault();
@@ -29,7 +43,8 @@ const App = () => {
     else{
       const person = {
         name:newName,
-        number:newNumber
+        number:newNumber,
+        id:persons.length +1
       }
       setPersons(persons.concat(person));
       setNewName('');
@@ -37,12 +52,16 @@ const App = () => {
     }
   }
 
+  
   const getNewName = (e) => setNewName(e.target.value) 
   const getNewNumber = (e) => SetNewNumber(e.target.value)
+  const getFilteredContact = (e) => setFilter(e.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
+      filter: <input onChange = {getFilteredContact} value = {filteredContact}/>
+      <h2>Add Contacts</h2>
       <form onSubmit = {addContact}>
         <div>
           name: <input onChange = {getNewName} value = {newName}/>
@@ -55,9 +74,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-     
-      {persons.map(person =><Contact key={person.name} name = {person.name} number = {person.number}/>)}
-     
+      <Contact persons = {persons} filteredContact = {filteredContact} />
       ...
     </div>
   )
