0f33d966cc7c2dfbe86b9160087a623dd333cbf6
kazumaneet <mekbibtare@gmail.com>
2021-12-03 17:13:53 +0300

    put the all the intial contacts in a a json file and retrieved it from the server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
 
 const Input = ({text,onChange,value}) => <div>{text} <input onChange = {onChange} value = {value}/> </div>
 const Form = ({onSubmit,getNewName,getNewNumber,newName,newNumber}) =>{
@@ -26,17 +27,19 @@ const Contact = ({persons,filteredContact}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('');
   const [newNumber,SetNewNumber] = useState('');
   const [filteredContact,setFilter] = useState('');
   
+  useEffect(()=>{
+      axios
+      .get('http://localhost:3001/persons')
+      .then(response =>{
+        const notes = response.data
+        setPersons(notes);
+      })
+  },[]) 
 
   const addContact = (e) =>{
     e.preventDefault();
